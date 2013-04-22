7ddb66f58cde44f079bf0b35c1a8ccf0ace6d52b
David King <dave@davbo.org>
2013-04-22 17:01:26 +0100

    Simple view to display river status

---
+++ moxie_river_status/views.py
@@ -0,0 +1,25 @@
+import logging
+
+from flask import request
+
+from moxie.core.views import ServiceView, accepts
+from moxie.core.representations import JSON, HAL_JSON
+
+from .representations import HALRiversStatusRepresentation
+from .services import RiverStatusService
+
+logger = logging.getLogger(__name__)
+
+
+class Status(ServiceView):
+
+    def handle_request(self):
+        service = RiverStatusService.from_context()
+        last_updated = service.get_last_updated()
+        status = service.get_status()
+        return status, last_updated
+
+    @accepts(HAL_JSON, JSON)
+    def as_hal_json(self, response):
+        return HALRiversStatusRepresentation(*response,
+                endpoint=request.url_rule.endpoint).as_json()
